2efde72c4f69071aadcd809c3847f13ce4fd7bd1
rahmad ramadhan <rahmadr2612@gmail.com>
2021-11-15 19:34:28 +0700

    file uts
    file uts

---
+++ UTS/UTS/Assets/Script/ObstacleGenerator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+    float time = 0;
+    float timer = 0.8f;
+    public GameObject rocks;
+    
+    void Update()
+    {
+        if (time <= 0)
+        {
+            Instantiate(rocks, transform.position, Quaternion.identity);
+            time = timer;
+        }
+        else
+        {
+            time -= Time.deltaTime;
+        }
+    }
+    
+}
